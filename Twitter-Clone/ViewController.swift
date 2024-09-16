2a7aee4807ba97c6baab8b73ce71e4fb3ff21073
Demerro <79833946+Demerro@users.noreply.github.com>
2024-09-16 12:41:42 +0300

    Initial commit

---
+++ Twitter-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Twitter-Clone
+//
+//  Created by Nikita Prokhorchuk on 16.09.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
