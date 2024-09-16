80e09f336a05a03986572864de5b611cf49ea06a
Demerro <79833946+Demerro@users.noreply.github.com>
2024-09-16 13:56:18 +0300

    Enable edge antialiasing

---
+++ Packages/TwitterApplicationUI/Sources/TwitterApplicationUI/ApplicationFlowController.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  TwitterApplicationUI
+//
+//  Created by Nikita Prokhorchuk on 16.09.24.
+//
+
+import Foundation
---
+++ Packages/TwitterMainUI/Sources/TwitterMainUI/MainFlowController.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  TwitterMainUI
+//
+//  Created by Nikita Prokhorchuk on 16.09.24.
+//
+
+import Foundation
